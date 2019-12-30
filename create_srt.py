e0b3c2eb5d2a2fde054fbeac85c1a8176bf788f3
Onur Kaya <46904292+varmchairDev@users.noreply.github.com>
2019-12-31 01:24:25 +0300

    Update create_srt.py

--- create_srt.py
+++ create_srt.py
@@ -3,7 +3,7 @@ def createSRTTemplate(vid_h,vid_m,vid_s):
   m = 0
   s = 0
   len = 0
-  vid_len = vid_h*60 + vid_m + vid_s/60
+  vid_len = vid_h*60 + vid_m + vid_s/60.0
   it = 1
   while len < vid_len:
     if (h < 10):
@@ -21,25 +21,30 @@ def createSRTTemplate(vid_h,vid_m,vid_s):
 
     print(it)
 
-    if (m < 59 and s < 57):
+    if (s < 57):
       if (s+3 < 10):
         print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":"+p_m+":0"+str(int(p_s)+3)+",000")
       else:
         print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":"+p_m+":"+str(int(p_s)+3)+",000")
       s += 3
     elif (m < 59 and s>= 57):
-      print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":"+str(int(p_m)+1)+":0"+str(int(p_s)+3-60)+",000")
+      if (m+1<10):
+        print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":0"+str(int(p_m)+1)+":0"+str(int(p_s)+3-60)+",000")
+      else:
+        print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":"+str(int(p_m)+1)+":0"+str(int(p_s)+3-60)+",000")
       m += 1
       s += 3 - 60
     elif (m == 59 and s >= 57):
-      print(p_h+":"+p_m+":"+p_s+",500"+" --> " + p_h+":"+"00"+":0"+str(int(p_s)+3-60)+",000")
+      if (h+1 < 10):
+        print(p_h+":"+p_m+":"+p_s+",500"+" --> 0" + str(int(p_h)+1)+":"+"00"+":0"+str(int(p_s)+3-60)+",000")
+      else:
+        print(p_h+":"+p_m+":"+p_s+",500"+" --> " + str(int(p_h)+1)+":"+"00"+":0"+str(int(p_s)+3-60)+",000")
       h += 1
       m = 0
       s += 3 - 60
 
-    len = h*60 + m + s/60
+    len = h*60 + m + s/60.0
     it += 1
     print("\n")
-
 
 #create srt template for 10 min length video: createSRTTemplate(0,10,0)
